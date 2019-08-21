f0861c15b8f4a9a618086f5d2ab2622d5e9f7082
Tuan Tran Van <hunter.tran.92@outlook.com>
2019-08-21 15:12:44 -0400

    remove unused regex

--- src/models/RegexStrings.ts
+++ src/models/RegexStrings.ts
@@ -4,7 +4,6 @@ export class RegexStrings {
     REGEXP_TOC_CONFIG = /\w+[:=][\w.]+/gi;
     REGEXP_TOC_CONFIG_ITEM = /(\w+)[:=]([\w.]+)/;
     REGEXP_MARKDOWN_ANCHOR = /^<a id="markdown-.+" name=".+"><\/a\>/;
-    REGEXP_HEADER = /^(\#{1,6})\s*(.+)/;
     REGEXP_CODE_BLOCK1 = /^```/;
     REGEXP_CODE_BLOCK2 = /^~~~/;
     REGEXP_ANCHOR = /\[.+\]\(#(.+)\)/;
