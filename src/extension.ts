alanwalk/markdown-toc

commit fe9b778c8a759b0d99e149dc6635fb2571cffcd1

update

src/extension.ts
@@ -21,9 +21,10 @@ const REGEXP_TOC_CONFIG         = /\w+[:=][\w.]+/gi;
 const REGEXP_TOC_CONFIG_ITEM    = /(\w+)[:=]([\w.]+)/;
 const REGEXP_MARKDOWN_ANCHOR    = /^<a id="markdown-.+" name=".+"><\/a\>/;
 const REGEXP_HEADER             = /^(\#{1,6})\s*([.0-9]*)\s*(.+)/;
-const REGEXP_CODE_BLOCK1         = /^```/;
-const REGEXP_CODE_BLOCK2         = /^~~~/;
+const REGEXP_CODE_BLOCK1        = /^```/;
+const REGEXP_CODE_BLOCK2        = /^~~~/;
 const REGEXP_ANCHOR             = /\[.+\]\(#(.+)\)/
+const REGEXP_IGNORE_TITLE       = /<!-- TOC ignore:true -->/
 
 const DEPTH_FROM                = "depthFrom";
 const DEPTH_TO                  = "depthTo";
@@ -352,6 +353,8 @@ class MarkdownTocTools {
             if (depth < this.options.DEPTH_FROM) continue;
             if (depth > this.options.DEPTH_TO) continue;
 
+            if (lineText.match(REGEXP_IGNORE_TITLE)) continue;
+
             for (var i = depth; i <= this.options.DEPTH_TO; i++) {
                 indicesOfDepth[depth] = 0;
             }
@@ -363,7 +366,9 @@ class MarkdownTocTools {
             }
 
             let title = lineText.substr(depth).trim();
-            title = title.replace(/\#*$/, "").trim(); 
+            title = title.replace(/\[(.+)]\([^)]*\)/gi, "$1");  // replace link
+            title = title.replace(/<!--.+-->/gi, "");           // replace comment
+            title = title.replace(/\#*_/gi, "").trim();         // replace special char
             
             if (hashMap[title] == null) {
                 hashMap[title] = 0

commit d1cbfd85a392006482f4b6063c5e59ab3ce5ed98

Remove bold and italic marks safely (only if at word boundaries)
Fixes wrong removal of _ in headers introduced in fe9b778

Fixes #30

src/extension.ts
@@ -126,7 +126,6 @@ class MarkdownTocTools {
         });
     }
 
-
     public updateMarkdownSections() {
         let tocRange = this.getTocRange();
         this.updateOptions(tocRange);
@@ -375,9 +374,10 @@ class MarkdownTocTools {
 
             let title = lineText.substr(depth).trim();
             let baseTitle = title.replace(/^(?:\d+\.)+/, "").trim(); // title without section number
-            title = title.replace(/\[(.+)]\([^)]*\)/gi, "$1");  // replace link
-            title = title.replace(/<!--.+-->/gi, "");           // replace comment
-            title = title.replace(/\#*_/gi, "").trim();         // replace special char
+            title = title.replace(/\[(.+)]\([^)]*\)/gi, "$1");       // replace link
+            title = title.replace(/<!--.+-->/gi, "");                // replace comment
+            title = title.replace(/\#/gi, "").trim();                // replace special char
+            title = title.replace(/\b[_*]|[*_]\b/gi, "");            // replace bold and italic marks
 
             if (!(title in hashMap)) {
                 hashMap[title] = 0;